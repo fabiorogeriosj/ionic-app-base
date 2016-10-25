3786e13a6b9fb66466a215c8df046a8a582cc791
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2016-10-25 15:47:37 -0200

    rotas

---
+++ js/config.js
@@ -0,0 +1,19 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  /*
+
+  $stateProvider
+
+
+    .state('exemplo', {
+      url: "/exemplo",
+      templateUrl: "exemplo.html",
+      controller:"exemploController"
+    })
+
+
+    $urlRouterProvider.otherwise('exemplo');
+
+    */
+
+})
